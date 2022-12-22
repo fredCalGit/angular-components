464b13b1ca7c0b3d606f58144f4843ee8a04b440
Frederico Muinhos <fred.cal@hotmail.com>
2022-12-22 11:24:10 -0500

    implemented table collection

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./collections-home.component.css']
 })
 export class CollectionsHomeComponent {
-
+  data = [
+    {
+      name: 'James', age: 25, job: 'Designer', employed: true
+    },
+    {
+      name: 'Jill', age: 27, job: 'Engineer', employed: true
+    },
+    {
+      name: 'Elyse', age: 21, job: 'Engineer', employed: false
+    },
+  ]
+  headers = [
+    { key: 'name', label: 'Name' },
+    { key: 'age', label: 'Age' },
+    { key: 'job', label: 'Job' },
+    { key: 'employed', label: 'Has a job?' },
+  ]
 }
